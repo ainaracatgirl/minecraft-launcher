9e05cbe542e6d69992f92df2958f7a1d987ec748
Ainara Garcia <ainaracatgirl@gmail.com>
2024-04-27 12:31:09 +0200

    make it work on non-debian

--- get-minecraft-client.sh
+++ get-minecraft-client.sh
@@ -29,33 +29,35 @@ if [[ $VERSION_JSON == "null" ]]; then
 fi
 
 # find the proper java - 8 before 1.13, 11 after
-javas=$(update-alternatives --list java)
-java8=$(echo "$javas" | grep -m1 "\-8.*java$" || true)
+#javas=$(update-alternatives --list java)
+#java8=$(echo "$javas" | grep -m1 "\-8.*java$" || true)
 #java11=$(echo "$javas" | grep -m1 java-11 || true)
-java16=$(echo "$javas" | grep -m1 "\-16.*java$" || true)
-java17=$(echo "$javas" | grep -m1 "\-17.*java$" || true)
-version_slug=$(echo $MAINLINE_VERSION | cut -d . -f 2)
-if [[ $version_slug -le 16 ]]; then
-    if [[ -z $java8 ]]; then
-        echo "Need Java 8 to run" #$MAINLINE_VERSION
-        exit 1
-    fi
-    JAVA=$java8
-elif [[ $version_slug -le 17 ]]; then
-    if [[ -z $java16 ]]; then
-        echo "Java 16 is required for mainline versions 1.17+. Only found these java versions: $javas"
-        exit 1
-    else
-        JAVA=$java16
-    fi
-else
-    if [[ -z $java17 ]]; then
-        echo "Java 17 is required for mainline versions 1.18+. Only found these java versions: $javas"
-        exit 1
-    else
-        JAVA=$java17
-    fi
-fi
+#java16=$(echo "$javas" | grep -m1 "\-16.*java$" || true)
+#java17=$(echo "$javas" | grep -m1 "\-17.*java$" || true)
+#version_slug=$(echo $MAINLINE_VERSION | cut -d . -f 2)
+#if [[ $version_slug -le 16 ]]; then
+#    if [[ -z $java8 ]]; then
+#        echo "Need Java 8 to run" #$MAINLINE_VERSION
+#        exit 1
+#    fi
+#    JAVA=$java8
+#elif [[ $version_slug -le 17 ]]; then
+#    if [[ -z $java16 ]]; then
+#        echo "Java 16 is required for mainline versions 1.17+. Only found these java versions: $javas"
+#        exit 1
+#    else
+#        JAVA=$java16
+#    fi
+#else
+#    if [[ -z $java17 ]]; then
+#        echo "Java 17 is required for mainline versions 1.18+. Only found these java versions: $javas"
+#        exit 1
+#    else
+#        JAVA=$java17
+#    fi
+#fi
+
+JAVA=java
 
 VERSION_DETAILS=$(curl -s $VERSION_JSON)
 mkdir -p versions/$MAINLINE_VERSION
